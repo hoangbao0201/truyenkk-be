9235d6ae6665cc4317d1621c0b54fd31cb7de261
hoangbao0201 <hoangbao020103@gmail.com>
2024-04-02 15:04:57 +0700

    Add Prisma

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  await app.listen(process.env.PORT || 4000);
+}
+bootstrap();
